d5897747f3175cdd36bec238c599426b39bb2b80
Surajtamatta <surajtamatta8379911589@gmail.com>
2025-09-16 20:36:59 +0530

    added new event

--- src/pages/event/index.jsx
+++ src/pages/event/index.jsx
@@ -27,6 +27,13 @@ const events = [
     titleimg: "/revolvetitle.png",
     link: "https://www.eventbrite.com/e/bal-khabra-revolve-tickets-1538613276729?aff=oddtdtcreator",
   },
+    {
+    date: "October 20ᵗʰ, 2025",
+    title: "The Ripped Bodice",
+    location: "Brooklyn,NY",
+    titleimg: "/revolvetitle.png",
+    link: "https://www.therippedbodice.com/brooklyn-events",
+  },
 ];
 
 const container = {
@@ -47,15 +54,14 @@ const item = {
 export default function EventsPage() {
   return (
     <Layout>
-      <div className="min-h-[90vh] bg-pink-50 pt-36 sm:pt-16 md:pt-20 px-4 sm:px-6">
-        <div className="text-center mb-12">
-          <h1 className="text-4xl font-adobejenson  text-[#f095d8]">
+      <div className="h-[90vh] bg-pink-50 p-4 sm:p-6 overflow-hidden">
+        <div className="text-center mb-8">
+          <h1 className="text-3xl font-adobejenson  text-[#f095d8]">
             Let’s Hang Out!
           </h1>
         </div>
 
         <div className="relative max-w-2xl mx-auto">
-
           {/* Vertical Line */}
           <div className="block absolute left-1/2 top-0 h-full w-[2px]  sm:w-1 bg-gray-300 transform -translate-x-1/2 z-0" />
 
@@ -63,15 +69,15 @@ export default function EventsPage() {
             variants={container}
             initial="hidden"
             animate="show"
-            className="space-y-2 relative z-10"
+            className="relative z-10"
           >
             {events.map((event, index) => {
-              const isLeft = index === 1; // middle one on the left
+              const isLeft = index % 2 !== 0; // middle one on the left
               return (
                 <motion.div
                   key={index}
                   variants={item}
-                  className={`w-1/2 sm:px-6 flex flex-col justify-center items-center mt-0  rounded relative ${
+                  className={`w-1/2 sm:px-6 flex flex-col gap-0 justify-center items-center mt-0  rounded relative ${
                     isLeft ? "ml-0 mr-auto pr-4" : "mr-0 ml-auto pl-4"
                   }`}
                 >
@@ -82,7 +88,7 @@ export default function EventsPage() {
                     `}
                   />
 
-                  <div className="w-full max-w-24 sm:max-w-32 aspect-[1/0.4] relative left-0 ">
+                  <div className="w-full max-w-18 sm:max-w-24 aspect-[1/0.4] relative left-0 ">
                     <Image
                       src={event.titleimg}
                       alt={event.title}
@@ -91,16 +97,16 @@ export default function EventsPage() {
                       className="flex justify-start"
                     />
                   </div>
-                  <div className="font-kievitSerifBold text-gray-800 font-semibold text-base sm:text-xl uppercase">
+                  <div className="font-kievitSerifBold text-gray-800 font-semibold text-base sm:text-lg uppercase">
                     {event.date}
                   </div>
-                  <div className="mt-1  leading-none text-sm sm:text-xl flex flex-col items-center gap-2  text-gray-600">
+                  <div className="mt-1  leading-none text-sm sm:text-lg flex flex-col items-center   text-gray-600">
                     <span className="font-medium  font-kievitSerif ">{event.title}</span>
                     <span className=" font-kievitSerif "> {event.location}</span>
                   </div>
 
                  <Link href={event.link}>
-                   <button class="group mt-3 sm:mt-4 text-xs sm:text-sm relative bg-[#f095d8] h-7 sm:h-9  hover:text-[#f095d8] hover:bg-white inline-flex  px-3 py-2 sm:px-4 sm:py-2  items-center justify-center overflow-hidden rounded-lg border border-neutral-200   font-medium text-neutral-600 transition-all [box-shadow:0px_2px_1px_#a3a3a3] active:translate-y-[2px] active:shadow-none">
+                   <button class="group mt-2 sm:mt-3 text-xs sm:text-sm relative bg-[#f095d8] h-6 sm:h-8  hover:text-[#f095d8] hover:bg-white inline-flex  px-3 py-2 sm:px-4 sm:py-2  items-center justify-center overflow-hidden rounded-lg border border-neutral-200   font-medium text-neutral-600 transition-all [box-shadow:0px_2px_1px_#a3a3a3] active:translate-y-[2px] active:shadow-none">
                      GET TICKETS
                    </button></Link>
                 </motion.div>
